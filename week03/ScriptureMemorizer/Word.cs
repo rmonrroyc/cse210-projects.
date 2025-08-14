f9735336973d2d8fa60ef56a1ed81492bcfccf73
Ricardo Monrroy <ricardomonrroycerna@gmail.com>
2025-08-14 19:49:22 -0400

    updates

---
+++ week03/ScriptureMemorizer/Word.cs
@@ -0,0 +1,26 @@
+public class Word
+{
+    private string _text;
+    private bool _hidden;
+
+    public Word(string text)
+    {
+        _text = text;
+        _hidden = false;
+    }
+
+    public void Hide()
+    {
+        _hidden = true;
+    }
+
+    public bool IsHidden()
+    {
+        return _hidden;
+    }
+
+    public string GetDisplayText()
+    {
+        return _hidden ? new string('_', _text.Length) : _text;
+    }
+}
